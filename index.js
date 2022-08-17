7beed0f44502db8b59c3ec642e7cb3733c053308
Roy Lopez <windup.dues-0f@icloud.com>
2022-08-17 18:22:11 -0300

    orginizing server.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const Datastore = require('nedb');
 const fetch = require('node-fetch');
 require('dotenv').config();
-const port = process.env.PORT || 3000;
 
 // Express Server
 const app = express();
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening at port ${port}`));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
